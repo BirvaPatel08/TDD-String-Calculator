181b2f5d5af6cf6b46818d9a35b5e55657c9066a
birvapatel08 <birvamendpara@gmail.com>
2024-07-31 02:23:06 +0530

    Separating concerns by using functions

--- StringCalculator.js
+++ StringCalculator.js
@@ -4,7 +4,13 @@ class StringCalculator{
     {
         if(numbers === '')
             return 0;
-        else{
+            
+        const {delimiter, normalizedNumbers} = this.parseInput(numbers);
+        const parsedNumbers = this.parseNumbers(normalizedNumbers,delimiter);
+        this.checkNegative(parsedNumbers);
+        return this.sum(parsedNumbers);
+}
+        parseInput(numbers){
             let delimiter = /[\n,]+/;
             if(numbers.startsWith('//'))
             {
@@ -12,27 +18,30 @@ class StringCalculator{
                 delimiter = new RegExp(numbers.substring(2,delimiterEnd));
                 numbers = numbers.substring(delimiterEnd+1);
             }
-            let sum = 0;
-            let negatives = [];
-            numbers.split(delimiter). filter(val=>val.trim() !== '').forEach(val=>{
-                 const number = parseInt(val.trim(),10);
-
-                 if(number<0)
-                 {
-                    negatives.push(number);
-                 }
-                 sum += number;
-            });
-            
-            if(negatives.length > 0)
-            {
-                throw new Error(`Negative numbers are not allowed: ${negatives.join(', ')}`);
-            }
-
-            return sum;
+            return {delimiter, normalizedNumbers: numbers};
         }
-      
+            
+                parseNumbers(numbers, delimiter)
+                {
+                    return numbers.split(delimiter).filter(val=>val.trim() !== '')
+                    .map(val=>parseInt(val.trim(),10));
+                }
+
+                checkNegative(numbers)
+                {
+                    const negatives = numbers.filter(num=>num<0);
+                    if(negatives.length>0)
+                    {
+                        throw new Error(`Negative numbers are not allowed: ${negatives.join(', ')}`);
+
+                    }
+                }
+
+                sum(numbers){
+                    return numbers.reduce((a,b)=>a+b,0);
+                }
+            
     }
 
- }
+ 
  module.exports = StringCalculator;
